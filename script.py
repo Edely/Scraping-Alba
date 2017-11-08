08c730447be91e6f500cd3015a012c9c6c5adc4c
edelygomes <edelygomes@gmail.com>
2017-11-08 18:04:23 -0300

    subindo lista de deputados e inicio das mudanças

--- script.py
+++ script.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
-import mechanize
+import sys, argparse, logging, os, random, urllib, urllib2, common
+import mechanize, urlparse, json, cookielib, re
+
+#CONSTANTS
+url_inicial = 'http://www.al.ba.gov.br/transparencia/prestacao-de-contas'
+
 
 '''
->abrir pagina
+> escolher deputado ou raspar todo o conteudo
+
+> abrir pagina
     http://www.al.ba.gov.br/transparencia/prestacao-de-contasscip
 
 > selecionar deputado ( Itera todas as 60 opçoes )
@@ -14,3 +24,31 @@ import mechanize
 > salvar em arquivo csv/sql
 '''
 
+br = mechanize.Browser()
+pagina = br.open(url_inicial).read()
+
+
+
+def abre_pagina(deputado):
+    br = mechanize.Browser()
+    pagina = br.open(url_inicial).read()
+    
+    if deputado == 'todos':
+        #pesquisa todos:
+    else:
+        #pesquisa so o deputado
+    
+
+def main(args):
+    br, cj = common.initialize_browser()
+    try:
+        abre_pagina(deputado)
+    except urllib2.URLError:
+        logging.critical("Erro na conexão com a internet, tente novamente mais tarde")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        logging.critical("Fechando o programa.")
+        sys.exit(0)
+        
+if __name__ == '__main__':
+    pass     
