9ea046af156e70fc7cee09727ddfdfdd45473962
edelygomes <edelygomes@gmail.com>
2017-12-11 17:50:02 -0300

    dados csv brutos

--- script.py
+++ script.py
@@ -5,34 +5,59 @@ from bs4 import BeautifulSoup
 import sys, argparse, logging, os, random, urllib, urllib2, common
 import mechanize, urlparse, json, cookielib, re
 from lista_deputados import deputados
-#CONSTANTS
-url_inicial = 'http://www.al.ba.gov.br/transparencia/prestacao-de-contas'
 
-#print('a')
 
-br = mechanize.Browser()
-try:
-    pagina = br.open(url_inicial, timeout=60).read()
-except(urllib2.URLError) as e:
-    print('erro de conexao 1')
-    print(e)
-    sys.exit(0)
+
+             
+def acessa_form(deputy, year, month, form, br, k=0):
+    form['deputado'] = [str(deputy)]
+    form['mes'] = [str(month)]
+    form['ano'] = [str(year)]
+
+    br.submit()
+    resposta = br.response().read()
+    print(resposta)
+    soup_dados = BeautifulSoup(resposta, "lxml") 
     
-
-for form in br.forms():
-    if form.attrs.get('action') == 'prestacao-de-contas':
-        br.form = form
-        
-    #print(form)
+    [strong.extract() for strong in soup_dados('strong')]
+    
+    [clearFloat.decompose() for clearFloat in soup_dados.find_all("div", {'class': 'clearFloat'})]
+         
+    
+    dados = soup_dados.find_all('div', 'linha-prop')
+    if len(dados) != 0:
+        nome_arquivo = "dados_alba_"+str(k)+".csv"
+        arquivo = open(nome_arquivo, "a+")
+            
+    i = 0
+    for item in dados:
+        if i != 0:
+            arquivo.write(str(k)+'-'+deputados[str(deputy)] +'-')
+            infos_item = item.find_all('div')
+            j = 1
+            for infos in infos_item:
+                a = infos.text.lstrip()
+                palavras = a.split()                
+                if j == 1:
+                    arquivo.write(palavras[0]+'- ')
+                    arquivo.write(palavras[2]+'- ')
+                elif j == 2:
+                    arquivo.write(a.strip().encode("utf-8") +'- ')
+                elif j == 3:
+                    a = a[3:]
+                    arquivo.write(a.strip().encode("utf-8"))
+                j = j + 1
+        i+=1
+        arquivo.write('\n')
+              
+    arquivo.close()
         
 def abre_pagina(args):
-    #print('b')
-    #print(args)
-    
+    url_inicial = 'http://www.al.ba.gov.br/transparencia/prestacao-de-contas'
     br = mechanize.Browser()
     try:
-        #print('c')
         pagina = br.open(url_inicial, timeout=60).read()
+        br.set_handle_robots(False)
     except(urllib2.URLError) as e:
         print('erro de conexao 2')
         print(e)
@@ -42,14 +67,23 @@ def abre_pagina(args):
         if form.attrs.get('action') == 'prestacao-de-contas':
             br.form = form
     
-    #print('e')
-    
-    #parametros_pesquisa = {'deputado': 'todos', 'ano': 'todos', 'mes': 'todos'}
-    
     if args.deputado != 'todos':
         deputy = str(args.deputado)
     else:
-        deputy = str(0)
+        for k,v in deputados.items():
+            try:
+                print(deputados[k])
+            
+                for form in br.forms():
+                    if form.attrs.get('action') == 'prestacao-de-contas':
+                        br.form = form
+                        
+                deputy = str(k)
+                year = str(0)
+                month = str(0)
+                acessa_form(deputy, year, month, form, br, k)
+            except Exception as e:
+                print(e)
         
     if args.ano != 'todos':
         year = str(args.ano)
@@ -60,28 +94,13 @@ def abre_pagina(args):
         month = str(args.mes)
     else:
         month = str(0)
-        
-    #enviar = br.submit()
-    #print('f')
-    
-    #aqui entrar o loop para acessar os dados especificamente
-    
-    #deputado        
-#    def itera_forms():        
-#        while(indice_deputados < len(deputados.items())):
-#            print(indice_deputado)
-        
-    #ano
-    #mes
-    
-    acessa_form(deputy, year, month, form, br)
-        
-    #print('g')
-    
-    pagina_resposta = br.response().read()
-    #print('h')
+    k=0
+    return {"deputy": deputy, "year": year, "month": month, "form": form, "br": br, "deputado": args.deputado}
     
 def main(args):
+    print('===========')
+    print(args)
+    
     br, cj = common.initialize_browser()
     try:
         abre_pagina(deputado)
@@ -103,54 +122,11 @@ if __name__ == '__main__':
     if args.lista == 'mostrar':
         for k,v in deputados.items():
             print( k + ' - ' + v)
+    print('++++++++++++')
+    print(args)
+    resultado = abre_pagina(args)
+    
+    print(resultado['deputy'], resultado['year'], resultado['month'], resultado['form'], resultado['br'], resultado['deputado'])
+    acessa_form(resultado['deputy'], resultado['year'], resultado['month'], resultado['form'], resultado['br'], resultado['deputado'])
                         
-                
-def acessa_form(deputy, year, month, form, br):
-    form['deputado'] = [str(deputy)]
-    form['mes'] = [str(month)]
-    form['ano'] = [str(year)]
-
-    enviar = br.submit()
-    #print('legal')
-    resposta = br.response().read()
-    #print('h')
-    soup_dados = BeautifulSoup(resposta, "html5lib") 
-    
-    [strong.extract() for strong in soup_dados('strong')]
-    
-    [clearFloat.decompose() for clearFloat in soup_dados.find_all("div", {'class': 'clearFloat'})]
-         
-    
-    dados = soup_dados.find_all('div', 'linha-prop')
-    if len(dados) != 0:
-        arquivo = open("dados_alba.csv", "a+")
-            
-    i = 0
-    for item in dados:
-        if i != 0:
-#            print(item)
-#            print(deputados[str(deputy)])
-            arquivo.write(deputados[str(deputy)] +', ')
-            infos_item = item.find_all('div')
-            j = 1
-            for infos in infos_item:
-                a = infos.text.lstrip()
-                palavras = a.split()                
-                if j == 1:
-                    arquivo.write(palavras[0]+', ')
-                    arquivo.write(palavras[2]+', ')
-#                    print(palavras[0])
-#                    print(palavras[2])
-                elif j == 2:
-                    arquivo.write(a.strip().encode("utf-8") +', ')
-#                    print(a.strip())
-                elif j == 3:
-                    a = a[3:]
-                    arquivo.write(a.strip().encode("utf-8"))
-#                    print(a.strip())
-                j = j + 1
-        i+=1
-        arquivo.write('\n')
-              
-    arquivo.close() 
-abre_pagina(args)
+   
