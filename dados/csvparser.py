90f148d7cb1d1a1cb38996678e44bded1ed91bc7
edelygomes <edelygomes@gmail.com>
2017-12-12 15:23:39 -0300

    dados atualizando

--- dados/csvparser.py
+++ dados/csvparser.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from os import listdir, makedirs
 from os.path import isfile, join
-import sys, os, csv 
+import sys, os, csv
 
 meses = [[],[],[],[],[],[],[],[],[],[],[],[]]
 
@@ -14,25 +14,29 @@ for row in arquivo_csv:
     if row != []:
         mes = row[2]
         meses[int(mes)-1].append(row)
-        
+
 for mes in meses:
     tamanho = len(mes)
-    print(tamanho)
-    for counter in range(0, tamanho):
-        #print(mes[counter])
-        #sys.exit(0)        
-        #print(mes[counter][0])
-        if counter != tamanho - 1:
-            if(mes[counter][0] == mes[counter+1][0]) and (mes[counter][3] == mes[counter+1][3]):
-                print('sim')
-                print(mes[counter])
-                print(mes[counter+1])
-#            else:
-#                print('nao')
+    #print(tamanho)
+    for linha in mes:
+        print(linha[0])
+        print('================')
+        
+    # for counter in range(0, tamanho):
+    #     #print(mes[counter])
+    #     #sys.exit(0)
+    #     #print(mes[counter][0])
+    #     if counter != tamanho - 1:
+    #         if((mes[counter][4] == ' Assessorias') or (mes[counter][4] == ' Consultorias') or (mes[counter][4] == ' Pesquisas') or (mes[counter][4] == ' Trabalhos Técnicos')):
+    #             if(mes[counter][0] == mes[counter+1][0]) and (mes[counter][3] == mes[counter+1][3]):
+    #                 novo_valor =  float(mes[counter+1][5]) + float(mes[counter][5])
+    #                 mes[counter][5] = novo_valor
+                    # print(mes[counter])
+                    # print(mes[counter][5])
+                    # print(mes[counter+1])
+                    # print(mes[counter+1][5])
+                    # print(novo_valor)
+                    # print(mes[counter][5])
 
-#    
-#    #print(mes)
-#    
-#    
-#    ['923801', 'Dep. Samuel Júnior', '12', ' 2017', ' Assessorias', ' 9750']
+#['923801', 'Dep. Samuel Júnior', '12', ' 2017', ' Assessorias', ' 9750']
 #['923801', 'Dep. Samuel Júnior', '12', ' 2017', ' Consultorias', ' 9750']
